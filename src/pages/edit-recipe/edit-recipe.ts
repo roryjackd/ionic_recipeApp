2e6ab77ff14acee47d16784759880f3f897d38f9
roryjackd <roryjackd@yahoo.com>
2018-05-23 15:15:36 -0600

    Synced form and html

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -15,6 +15,11 @@ export class EditRecipePage implements OnInit {
 
   ngOnInit() {
     this.mode = this.navParams.get('mode');
+    this.initializeForm();
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
   }
 
   private initializeForm() {
