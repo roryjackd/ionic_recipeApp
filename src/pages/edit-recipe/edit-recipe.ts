bf33a927d4ff33e43340e31eb5dac3cb6cf5bdb5
roryjackd <roryjackd@yahoo.com>
2018-05-23 14:15:48 -0600

    Created reactive form

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { NavParams } from "ionic-angular";
 
 @Component({
@@ -8,10 +9,19 @@ import { NavParams } from "ionic-angular";
 export class EditRecipePage implements OnInit {
   mode = 'New';
   selectOptions = ['Easy', 'Medium', 'Hard'];
+  recipeForm: FormGroup;
 
   constructor (private navParams: NavParams) {}
 
   ngOnInit() {
     this.mode = this.navParams.get('mode');
+  }
+
+  private initializeForm() {
+    this.recipeForm = new FormGroup({
+      'title': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'difficulty': new FormControl('Medium', Validators.required)
+    });
   }
 }
