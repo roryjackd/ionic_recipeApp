b9a1c7d526ea69378c37a0646a01977187d69112
roryjackd <roryjackd@yahoo.com>
2018-05-29 14:27:19 -0600

    Set up firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Platform, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { SigninPage } from "../pages/signin/signin";
@@ -17,6 +18,10 @@ export class MyApp {
   @ViewChild('nav') nav: NavController;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCr9FnEwZksRV7b2Iov7u8QZhaY-xb10G0",
+      authDomain: "ionic2-recipebook-79631.firebaseapp.com"
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -31,7 +36,7 @@ export class MyApp {
   }
 
   onLogout() {
-    
+
   }
 }
 
