744253538b93cd0126b087368069f6903b5cfe6f
roryjackd <roryjackd@yahoo.com>
2018-05-28 22:14:53 -0600

    Added side menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
 import { TabsPage } from '../pages/tabs/tabs';
 import { ShoppingListService } from "../services/shopping-list";
 import { RecipesService } from "../services/recipes";
+import { SignupPage} from "../pages/signup/signup";
+import { SigninPage} from "../pages/signin/signin";
 
 
 @NgModule({
@@ -21,7 +23,9 @@ import { RecipesService } from "../services/recipes";
     RecipePage,
     RecipesPage,
     ShoppingListPage,
-    TabsPage
+    TabsPage,
+    SigninPage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
@@ -34,7 +38,9 @@ import { RecipesService } from "../services/recipes";
     RecipePage,
     RecipesPage,
     ShoppingListPage,
-    TabsPage
+    TabsPage,
+    SignupPage,
+    SigninPage
   ],
   providers: [
     StatusBar,
