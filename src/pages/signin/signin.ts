1b5c70e0fd128ef8d58f2f45ebb9d03015de453a
roryjackd <roryjackd@yahoo.com>
2018-05-29 20:38:03 -0600

    Refined signin process

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from "@angular/forms";
+import { LoadingController, AlertController } from "ionic-angular";
+
 import { AuthService } from "../../services/auth";
 
 @Component({
@@ -8,15 +10,29 @@ import { AuthService } from "../../services/auth";
 })
 export class SigninPage {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,
+              private loadingCtrl: LoadingController,
+              private alertCtrl: AlertController) {
+
+  }
 
   onSignin(form: NgForm) {
+    const loading = this.loadingCtrl.create({
+      content: 'Signing you in...'
+    });
+    loading.present();
     this.authService.signin(form.value.email, form.value.password)
       .then(data => {
-        console.log(data);
+        loading.dismiss();
       })
       .catch(error => {
-        console.log(error);
+        loading.dismiss();
+        const alert = this.alertCtrl.create({
+          title: 'Signin failed!',
+          message: error.message,
+          buttons: ['Ok']
+        });
+        alert.present();
       });
   }
 }
